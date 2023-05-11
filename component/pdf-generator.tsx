9db0e63e59bfff376d23ba52729306b6d17da6a5
harshith <harshith@qburst.com>
2023-05-11 13:12:19 +0530

    fixed issue with the build generation for deplaying to Vercel

--- component/pdf-generator.tsx
+++ component/pdf-generator.tsx
@@ -1,8 +1,8 @@
 import Image from 'next/image'
 // import './globals.css'
 import React from "react";
-import { jsPDF,HTMLOptionImage } from "jspdf";
-import { toPng,toCanvas } from "html-to-image";
+import { jsPDF, HTMLOptionImage } from "jspdf";
+import { toPng, toCanvas } from "html-to-image";
 type props = {
 
   html?: React.MutableRefObject<HTMLDivElement>;
@@ -11,24 +11,31 @@ type props = {
 
 const GeneratePdf: React.FC<props> = ({ html }) => {
   const generatePdf = () => {
-      const doc = new jsPDF();
-      doc.output("dataurlnewwindow");  
-      doc.save()
+    const doc = new jsPDF();
+    doc.output("dataurlnewwindow");
+    doc.save()
   };
 
-  const generateImage=async ()=>{
-    const image = await toPng(document.getElementById('bill'),{quality:0.95});
-    const doc = new jsPDF();
+  const generateImage = async () => {
+    if (document.getElementById('bill')) {
+      const image = await toPng(document.getElementById('bill'), { quality: 0.95 });
+      const doc = new jsPDF();
 
-      doc.addImage(image,'JPEG',5,22,200,160);
-      doc.save();
-
+      doc.addImage(image, 'JPEG', 5, 22, 200, 160);
+      var string = doc.output('datauristring');
+      var iframe = "<iframe width='100%' height='100%' src='" + string + "'></iframe>"
+      var x = window.open();
+      x.document.open();
+      x.document.write(iframe);
+      x.document.close();
+      doc.save()
+    }
 
   }
   return (
 
     <div className="button-container">
-        <button onClick={generateImage}>
+      <button onClick={generateImage}>
         Get PDF using image
       </button>
       {/* <button onClick={generatePdf}>
