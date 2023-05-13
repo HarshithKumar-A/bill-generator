42aad789e35a351448fb5eea5519a11cfc73db32
harshith <harshith@qburst.com>
2023-05-13 20:50:29 +0530

    escn

--- next.config.js
+++ next.config.js
@@ -1,19 +1,5 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//    reactStrictMode: true,
-// }
-// module.exports = nextConfig
-const withPWA = require('next-pwa');
-
-const settings = {
-  env: {
-  },
-  devIndicators: {
-    autoPrerender: false,
-  },
-  pwa: {
-    dest: 'public',
-  },
-};
-
-module.exports = settings;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+module.exports = nextConfig
